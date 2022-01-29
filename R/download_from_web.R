d9f8a42aaaaca3efbf8b4867cbcf10427a3cf991
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2022-01-29 15:29:37 +0100

    Update download_from_web.R

--- R/download_from_web.R
+++ R/download_from_web.R
@@ -1,11 +1,11 @@
 # the following code enables user to download the table that we display in the markdown
 
-```ruby
 # code for enabling csv download
 write.csv2(uicn, "./file.csv")
 readLines("./file.csv") %>% 
   paste0(collapse="\n") %>% 
   openssl::base64_encode() -> encoded
 
+# call download
 [Download CSV](`r sprintf('data:text/csv;base64,%s', encoded)`)
- ```
+ 
