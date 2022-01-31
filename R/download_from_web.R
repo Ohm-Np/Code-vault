d69fc39657198d969d39c29b8a0b14fa580392f6
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2022-01-31 13:08:35 +0100

    add plotly functions

--- R/download_from_web.R
+++ R/download_from_web.R
@@ -1,4 +1,4 @@
-# the following code enables user to download the table that we display in the markdown
+# the following code enables user to download the table that we display in the markdown ----
 
 # code for enabling csv download
 write.csv2(uicn, "./file.csv")
@@ -9,3 +9,12 @@ readLines("./file.csv") %>%
 # call download
 [Download CSV](`r sprintf('data:text/csv;base64,%s', encoded)`)
  
+# use plotly ----
+library(plotly)
+
+# prepare plot object - the data should be provided as long table
+fig1 <- plot_ly(uicn_long.final, x = ~year, y = ~value,
+                name = ~Name, color = ~Name, type = 'scatter', mode = 'lines+markers') %>%
+  layout(title="Forst cover loss area in ha")
+# view the plot
+fig1
