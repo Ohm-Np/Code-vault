0b18280b4c9b50ebb126f4cefd95f0e6d95359b4
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2022-02-27 22:13:34 +0100

    Create ggplot2_erosivity.R

---
+++ R/ggplot2_erosivity.R
@@ -0,0 +1,18 @@
+# plot the vertical bar plot between two variables 
+g <- ggplot(erosivity, aes(x=reorder(Station_Name, -ROSE_Index), y=ROSE_Index, fill = Station_Name)) +
+  geom_bar(stat="identity", width=.5, fill="wheat") +
+  labs(title="Monthly Rainfall Erosivity",
+       x = "Station Name",
+       y = "Soil Erosivity (MJmmha-1h-1)",
+       subtitle="Station vs R-factor plot") +
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+g
+
+# adds horizontal line which denotes threshold 
+gv <- g + geom_hline(yintercept =  20000, linetype = "dashed", color = "orangered", size=1)
+gv
+
+# flip to create horizontal bar plot
+gv + coord_flip()
+
+# Note: reorder() allow us to plot either in ascending or descending order
