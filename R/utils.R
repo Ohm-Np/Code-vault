2a28abd83c4941fec0209a81a2ecbb898f0d6c83
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2022-03-21 17:41:27 +0100

    Create utils.R

---
+++ R/utils.R
@@ -0,0 +1,18 @@
+# the chunk of codes here contain mini/micro scripts that might ...
+# ... come handy while solving string related issues
+
+# (Q1) get particular string or numbers from a single string
+# for e.g. we have a string named "om.bhandari-1995/03/22.txt"
+# Now, I want following three things:-
+
+## 1. 19950322
+a = strsplit(name, "-|.txt")[[1]][2]
+b = strsplit(a, "/")
+paste(b[[1]][1],b[[1]][2],b[[1]][3],sep = "")
+
+## 2. 1995/03/02
+strsplit(name, "-|.txt")[[1]][2]
+
+## 3. om.bhandari
+strsplit(name, "-")[[1]][1]
+
