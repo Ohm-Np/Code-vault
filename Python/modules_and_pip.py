89135f8101dbfb88efbeec9ba1092786f3fc0af9
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2023-02-08 14:20:22 +0100

    Add files via upload

---
+++ Python/modules_and_pip.py
@@ -0,0 +1,10 @@
+# using modules in python
+# a module is essentially just a python file that we can ...
+# ... import into current python file
+
+import tools_for_modules
+print(tools_for_modules.roll_dice(10))
+
+# pip - package manager (preferred installer program)
+# install, manage, update and uninstall different package module
+
