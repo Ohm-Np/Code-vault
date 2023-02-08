a2823f6e556b1f296b0ca2f5e5b4f25b0d7df1d3
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2023-02-08 13:02:07 +0100

    Add files via upload

---
+++ Python/try_except.py
@@ -0,0 +1,10 @@
+# error handling
+
+try:
+    value = 10 / 0
+    num = int(input("Enter a number: "))
+    print(num)
+except ZeroDivisionError as err:
+    print(err)
+except ValueError:
+    print("Invalid input")
