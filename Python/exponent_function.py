c58b122db90504548ec8505d47f4268ee53a8153
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2023-02-08 12:09:06 +0100

    Add files via upload

---
+++ Python/exponent_function.py
@@ -0,0 +1,13 @@
+# **
+
+# two raised to the third power
+# print(2**3)
+
+# use for loops to create exponent function
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return(result)
+
+print(raise_to_power(2, 4))
